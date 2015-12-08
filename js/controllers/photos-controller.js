c046c58267a7c455ff84d2ec55210c356349db2a
Alex <alex.silva@batanga.com>
2015-12-08 09:49:27 -0200

    filters and css animation

--- js/controllers/photos-controller.js
+++ js/controllers/photos-controller.js
@@ -1,5 +1,8 @@
 angular.module("alexpic").controller("PhotosController", function($scope, $http) {
 	$scope.photos = [];
+	$scope.filter = "";
+	
+	// getting the photos
 	$http.get("api/calls.php?action=photos").success(function successCallback(data) {
 		$scope.photos = data;
 	
