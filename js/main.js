0a6570516ac85775415ec72ebd195fb0bf4340c8
Alex <alex.silva@batanga.com>
2015-12-08 12:15:47 -0200

    using ngRoute to create app routes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-angular.module("alexpic", ['ngMaterial','customDirectives']);
+angular.module("alexpic", ['ngMaterial','customDirectives','ngRoute'])
+.config(function($routeProvider, $locationProvider) {
+	
+	$locationProvider.html5Mode(true);
+
+	$routeProvider.when("/photos", {
+		templateUrl : "partials/photos.html",
+		controller : "PhotosController"
+	});
+
+	$routeProvider.when("/photos/new", {
+		templateUrl : "partials/edit-photo.html"
+	});
+
+	$routeProvider.otherwise({redirectTo : "/photos"});
+});
