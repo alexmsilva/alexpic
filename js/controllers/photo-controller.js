3a57e3710e4220d2c131f85aa9f9f15e7400a091
Alex <alex.silva@batanga.com>
2015-12-16 11:25:47 -0200

    creating a promise in a service

--- js/controllers/photo-controller.js
+++ js/controllers/photo-controller.js
@@ -1,4 +1,4 @@
-angular.module("alexpic").controller("PhotoController", function($scope, $mdToast, $routeParams, photoResource) {
+angular.module("alexpic").controller("PhotoController", function($scope, $mdToast, $routeParams, photoResource, photoSavior) {
 	/* to avoid always to send an action, you may use http methods
 		GET to get the images
 		PUT to insert a new image
@@ -9,40 +9,15 @@ angular.module("alexpic").controller("PhotoController", function($scope, $mdToas
 	$scope.photo = {};
 	$scope.savePhoto = function() {
 		if ($scope.photoForm.$valid) { // Hey! How can I access this?
-			if ($scope.photo._id) {
-				photoResource.update($scope.photo, function() {
+			photoSavior.save($scope.photo).then(function(response) {
+				$scope.showMessage(response.message);
+				if (response.inclusion) {
 					$scope.photo = {};
-					$scope.showMessage("A Foto foi alterada com sucesso!");
-
-				}, function(data, status) {
-					$scope.showMessage("Não foi possível alterar a foto.");
-				});
-				/*$http.post("api/photos.php?action=edit", $scope.photo)
-				.success(function(data) {
-					$scope.photo = {};
-					$scope.showMessage("A Foto foi alterada com sucesso!");
-				})
-				.error(function(data, status) {
-					$scope.showMessage("Não foi possível alterar a foto.");
-				});*/
-			}
-			else {
-				photoResource.newPhoto($scope.photo, function() {
-					$scope.photo = {};
-					$scope.showMessage("Foto adicionada com sucesso!");
-
-				}, function(data, status) {
-					$scope.showMessage("Não foi possível cadastrar a foto.");
-				});
-				/*$http.post("api/photos.php?action=new", $scope.photo)
-				.success(function(data) {
-					$scope.photo = {};
-					$scope.showMessage("Foto adicionada com sucesso!");
-				})
-				.error(function(data, status) {
-					$scope.showMessage("Não foi possível cadastrar a foto.");
-				});*/
-			}
+				};
+			})
+			.catch (function(response) {
+				$scope.showMessage(response.message);
+			});
 		};
 	};
 
--- js/services/custom-services.js
+++ js/services/custom-services.js
@@ -4,4 +4,42 @@ angular.module("aServices", ['ngResource'])
 		newPhoto : { method : "PUT" },
 		update : { method : "POST" }
 	});
+})
+.factory("photoSavior", function(photoResource, $q) {
+	var service = {};
+	service.save = function(photo) {
+		return $q(function(resolve, reject) {
+			if (photo._id) {
+				photoResource.update(photo, function() {
+					resolve({
+						message : "A foto " + photo.title + " foi alterada com sucesso!",
+						inclusion : false
+					});
+
+				}, function(data, status) {
+					console.log(data);
+					reject({
+						message : "Não foi possível alterar a foto " + photo.title
+					});
+				});
+			}
+			else {
+				photoResource.newPhoto(photo, function() {
+					resolve({
+						message : "A foto " + photo.title + " foi incluída com sucesso!",
+						inclusion : true
+					});
+
+				}, function(data, status) {
+					console.log(data);
+					reject({
+						message : "Não foi possível cadastrar a foto " + photo.title
+					});
+				});
+			};
+		
+		}); //$q
+	}
+
+	return service;
 });
